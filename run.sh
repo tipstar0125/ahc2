3fa990de518b496dabe5ac58b2ec23d80f0f425a
Yoke <06106186yoke@gmail.com>
2024-11-03 14:01:23 +0900

    スクリプトにrustのバージョン指定

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 clear >$(tty)
 echo ""
 export RUSTFLAGS=-Awarnings
-cargo run -r --features local --bin $1 < tools/in/$2.txt > tools/out/$2.txt
+cargo +1.70-x86_64-unknown-linux-gnu run -r --features local --bin $1 < tools/in/$2.txt > tools/out/$2.txt
 cp tools/in/$2.txt in
 cp tools/out/$2.txt out
--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 clear >$(tty)
 echo ""
 export RUSTFLAGS=-Awarnings
-cargo test -r --features local --bin $1 -- --nocapture < in
+cargo +1.70-x86_64-unknown-linux-gnu test -r --features local --bin $1 -- --nocapture < in
