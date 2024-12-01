25d5826be510a07d249d97f210004b0728054834
tipstar0125 <06106186yoke@gmail.com>
2024-12-01 14:01:00 +0900

    レスポンスをデバッグ出力

--- src/bin/ahc040/main.rs
+++ src/bin/ahc040/main.rs
@@ -10,12 +10,13 @@ use std::collections::VecDeque;
 
 use common::get_time;
 use input::{read_input, Input};
+use proconio::input_interactive;
 
 const HOW_TO_PACK: [(bool, char); 4] = [(false, 'U'), (true, 'U'), (false, 'L'), (true, 'L')];
 
 fn solve(input: &Input) {
-    let dt = (7e5 as i32 - 5e4 as i32) / input.T as i32;
-    let mut width_limit = 5e4 as i32;
+    let dt = (7e5 as i32 - 2e5 as i32) / input.T as i32;
+    let mut width_limit = 2e5 as i32;
     for _ in 0..input.T {
         width_limit += dt;
         let mut ans = "".to_string();
@@ -59,6 +60,11 @@ fn solve(input: &Input) {
             now = pos + 1;
         }
         println!("{}", ans);
+        input_interactive! {
+            w: usize,
+            h: usize,
+        }
+        eprintln!("limit={}, w = {}, h = {}, l={}", width_limit, w, h, w + h);
     }
 }
 
