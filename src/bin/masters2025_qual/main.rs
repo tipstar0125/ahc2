d8e16c2a4d83cc8be02e04e170cd49283b3770ec
tipstar0125 <06106186yoke@gmail.com>
2025-03-01 16:34:26 +0900

    initialize

---
+++ src/bin/masters2025_qual/main.rs
@@ -0,0 +1,18 @@
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+use input::Input;
+
+use crate::{common::get_time, input::read_input};
+
+mod common;
+mod input;
+
+fn solve(input: &Input) {}
+
+fn main() {
+    get_time();
+    let input = read_input();
+    solve(&input);
+    eprintln!("Elapsed time = {:.3}", get_time());
+}
